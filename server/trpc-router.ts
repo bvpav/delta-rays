a049ae9e507e1606fbe9c9e204afba9de0bfa679
bvpav <bvpav@tutanota.com>
2022-10-01 16:20:48 +0300

    Added images from JWST API

--- server/trpc-router.ts
+++ server/trpc-router.ts
@@ -13,65 +13,18 @@ export const appRouter = t.router({
       })
     )
     .query(async ({ input }) => {
-      const url = new URL('https://www.nasa.gov/api/2/ubernode/_search');
-      url.searchParams.set('size', '5');
-      url.searchParams.set('from', input.page.toString());
-      url.searchParams.set('sort', 'promo-date-time:desc');
-      url.searchParams.set('q', '((ubernode-type:image) AND (missions:3698))');
-      url.searchParams.set(
-        '_source_include',
-        'promo-date-time,master-image,nid,title,topics,missions,collections,other-tags,ubernode-type,primary-tag,secondary-tag,cardfeed-title,type,collection-asset-link,link-or-attachment,pr-leader-sentence,image-feature-caption,attachments,uri'
-      );
-      const res = await fetch(url);
+      const url = new URL('https://api.jwstapi.com/all/type/jpg');
+      url.searchParams.set('page', (input.page + 1).toString());
+      url.searchParams.set('perPage', '5');
+      const res = await fetch(url.toString(), {
+        headers: {
+          'X-API-KEY': process.env.JWSTAPI_TOKEN!,
+        },
+      });
       const obj = (await res.json()) as {
-        took: number;
-        timed_out: boolean;
-        _shards: {
-          total: number;
-          successful: number;
-          skipped: number;
-          failed: number;
-        };
-        hits: {
-          total: number;
-          max_score: any;
-          hits: {
-            _type: string;
-            _id: string;
-            _score: any;
-            _source: {
-              'image-feature-caption': string;
-              nid: string;
-              title: string;
-              type: string;
-              uri: string;
-              collections: string[];
-              'link-or-attachment': 'link' | 'attachment';
-              missions: string[];
-              'primary-tag': string;
-              'cardfeed-title': string;
-              'promo-date-time': string;
-              'secondary-tag': string;
-              'master-image': {
-                fid: string;
-                alt: string;
-                width: string;
-                id: string;
-                title: string;
-                uri: string;
-                height: string;
-              };
-              'ubernode-type': string;
-            };
-            sort: number[];
-          }[];
-        };
+        body: { location: string; thumbnail: string }[];
       };
-      return obj.hits.hits.map(
-        ({ _source }) =>
-          'https://www.nasa.gov/sites/default/files/styles/full_width_feature/public' +
-          new URL(_source['master-image'].uri.replace('://', ':///')).pathname
-      );
+      return obj.body.map((o) => o.location);
     }),
 });
 
